f89e26ec5b067222fd0d497123bac50b0af41145
adamop8 <45922406+adamop8@users.noreply.github.com>
2023-01-28 21:53:48 +0200

    main.py changed to server.py, and code planning and template was add

---
+++ server.py
@@ -0,0 +1,52 @@
+# server.py
+# 
+# Date: 28.1.2023
+# Author: Adam ofer
+#
+# A server handler to doomy.
+# the handle will be done using flags or menu input.
+
+# Imports
+
+# Constants
+
+def main(): # doomy Learn Assembly -a  # doomy Learn Assembly --add
+    print("Doomy\n"
+          "---------\n"
+          "1. Add a doomy\n"
+          "2. view all of the doomys\n"
+          "3. change doomy state\n"
+          "\n")
+    
+    
+def add():
+    """
+    add a new doomy
+    """
+    print()
+
+
+def view():
+    """
+    View all the doomys
+    """
+    print()
+
+
+def change_state():
+    """
+    requirments:
+       1. choose a to do by its number
+       2. states:
+           To do
+           In progress
+           done
+        
+        the changing will be done with option the set the state,
+        and more simple move one state forwards or backwards
+    """
+    print()
+
+    
+if __name__ == "__main__":
+    main()
